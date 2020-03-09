8c3c6fccfb6ca572ea90cafc9f7086286fdc3c2d
gaoxiaowei <gaoxiaoweiandy@163.com>
2020-03-09 18:03:15 +0800

    init

---
+++ app/src/main/java/com/dongnao/lsn5_paint_xfermode/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dongnao.lsn5_paint_xfermode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
